b8bcb0264a8f597e03ad210b273d298af8c95bbc
Andrew Harper <druharper@gmail.com>
2020-04-15 15:02:57 -0500

    premerge master

--- src/scripts/news/NewsForm.js
+++ src/scripts/news/NewsForm.js
@@ -1,39 +1,42 @@
 const eventHub = document.querySelector('.container')
 const contentTarget = document.querySelector('#newsForm')
+// const newsContent = document.querySelector('.news')
 
 let visibility = false
 
-// eventHub.addEventListener('newsFormButtonClicked', (customEvent) => {
-//   visibility = !visibility
+eventHub.addEventListener('newsFormButtonClicked', (customEvent) => {
+  visibility = !visibility
 
-//   if (visibility) {
-//     contentTarget.classList.remove('hide')
-//   } else {
-//     contentTarget.classList.add('hide')
-//   }
-// })
+  if (visibility) {
+    contentTarget.classList.remove('hide')
+  } else {
+    contentTarget.classList.add('hide')
+  }
+})
 
 // Handle browser-generated click event in component
-// contentTarget.addEventListener('click', (clickEvent) => {
-//   if (clickEvent.target.id === 'addNews') {
-//     const newsTitle = document.querySelector('#newsTitle').value
-//     const newsSynopsis = document.querySelector('#newsSynopsis').value
-//     const newsUrl = document.querySelector('#newsUrl').value
+contentTarget.addEventListener('click', (clickEvent) => {
+  if (clickEvent.target.id === 'addNews') {
+    const newsTitle = document.querySelector('#newsTitle').value
+    const newsSynopsis = document.querySelector('#newsSynopsis').value
+    const newsUrl = document.querySelector('#newsUrl').value
 
-//     // Make a new object representation of a news
-//     const newNews = {
-//       newsTitle: newsTitle,
-//       newsSynopsis: newsSynopsis,
-//       newsUrl: newsUrl,
-//       // timestamp: Date.now(),
-//     }
+    // Make a new object representation of a news
+    const newNews = {
+      newsTitle: newsTitle,
+      newsSynopsis: newsSynopsis,
+      newsUrl: newsUrl,
+      // timestamp: Date.now(),
+    }
 
-//     // Change API state and application state
-//     saveNews(newNews)
-//   }
-// })
+    // Change API state and application state
+    saveNews(newNews)
+  }
+})
 
 const render = () => {
+  const contentTarget = document.querySelector('#newsForm')
+
   // contentTarget.classList.add('hide')
   // const allNews = useNews()
 
