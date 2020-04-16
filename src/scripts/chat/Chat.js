4282fe34c9e56fb87cf69829872c048a78a7859d
iamdrewkirkland <drew.kirkland@gmail.com>
2020-04-16 16:31:26 -0400

    Each chat message renders a + next to the username which will add them as a friend if applicable.

--- src/scripts/chat/Chat.js
+++ src/scripts/chat/Chat.js
@@ -1,7 +1,26 @@
+const eventHub = document.querySelector(".container")
+// const contentTarget = document.querySelector(".chatPreview__message")
+
 export const ChatHTML = (message, user) => {
+    const username = user.username
+    
     return `
         <div class="chatPreview__message">
-            <p id="chatUser">${user.username}</p><p>${message.message}</p>
+            <p id="chatUser">${username}<button value=${username} class="addChatFriend">+</button></p><p>${message.message}</p>
         </div> 
     `
 }
+
+// event listener that calls "add friend" on button click
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "addChatFriend") {
+            
+            const addNewFriendEvent = new CustomEvent("AddNewFriend", {
+                detail: {
+                    friendName: clickEvent.target.value
+                }
+            })
+            eventHub.dispatchEvent(addNewFriendEvent)
+    }
+
+})
