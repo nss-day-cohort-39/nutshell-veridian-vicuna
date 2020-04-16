2469a79ec47bfde36b1e29d1dc3f86e051c84d9f
Brant2001 <brant.pippin@gmail.com>
2020-04-16 14:47:06 -0500

    added and event listener in TaskForm.js to save a new task when the save task button is clicked

--- src/scripts/tasks/TaskForm.js
+++ src/scripts/tasks/TaskForm.js
@@ -3,30 +3,48 @@
     representation of the task form 
 */
 
+// Imports
+import { saveTask } from "./TaskProvider.js"
 
-const contentTarget = document.querySelector(".tasks")
+
+// Give variable names to desired targets
 const eventHub = document.querySelector(".container")
 
-let visibility = false
 
-// Create an event listener that will display the task form to appear in the task container when heard
-eventHub.addEventListener("noteFormButtonClicked", customEvent => {
-    visibility = !visibility
-    
-    if(visibility){
-        contentTarget.classList.remove("invisible")
+// Event listener that will save a new object to our database when the save button is heard
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveTask") {
+
+        const taskText = document.querySelector("#taskText").value
+        const timeStamp = document.querySelector("#timeStamp").value
+        const currentUserId = sessionStorage.getItem('activeUser')
+
+        // Create the format for a new task object
+        const newTask = {
+            userId: parseInt(currentUserId),
+            task: taskText,
+            expectDone: timeStamp,
+            complete: "false"
+        }
+
+        saveTask(newTask)
     }
-    else{
-        contentTarget.classList.add("invisible")
-    }
+})
+
+
+// Event listener that will display the form in the task container when the add button is heard
+eventHub.addEventListener("taskFormButtonClicked", customEvent => {
+    const taskFormTarget = document.querySelector(".taskFormContainer")
+    taskFormTarget.classList.remove("hide")
     
 })
+
 
 // The Task form HTML
 export const TaskForm = () => {
 
-    contentTarget.innerHTML = `
-        <section class="taskFormContainer">
+    return `
+        <section class="taskFormContainer hide">
             <fieldset>
                 <label for="taskText">Task:</label>
                 <input type="text" id="taskText">
