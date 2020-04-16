7f0cebe038f653c6f35a53e4db8dd94e31fb27b7
iamdrewkirkland <drew.kirkland@gmail.com>
2020-04-16 17:07:34 -0400

    Add chat friend button now responds to click and prompts with appropriate message depending on friend status of user.

--- src/scripts/chat/Chat.js
+++ src/scripts/chat/Chat.js
@@ -6,15 +6,14 @@ export const ChatHTML = (message, user) => {
     
     return `
         <div class="chatPreview__message">
-            <p id="chatUser">${username}<button value=${username} class="addChatFriend">+</button></p><p>${message.message}</p>
+            <p id="chatUser">${username}<button value=${username} id="addChatFriend--${user.id}">+</button></p><p>${message.message}</p>
         </div> 
     `
 }
 
 // event listener that calls "add friend" on button click
 eventHub.addEventListener("click", clickEvent => {
-    if (clickEvent.target.id === "addChatFriend") {
-            
+    if (clickEvent.target.id.startsWith("addChatFriend")) {
             const addNewFriendEvent = new CustomEvent("AddNewFriend", {
                 detail: {
                     friendName: clickEvent.target.value
