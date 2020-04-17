2dec5d3c8c6cae5db276b1463c4a710b7ccd87ce
Andrew Harper <druharper@gmail.com>
2020-04-17 10:46:33 -0500

    Added planning boards

--- src/scripts/friends/Friend.js
+++ src/scripts/friends/Friend.js
@@ -5,36 +5,38 @@
     Authors: Heidi Sprouse
 */
 
-import { deleteFriend } from "./friendProvider.js"
-import { useUsers } from "../users/userProvider.js"
+import { useUsers } from '../users/userProvider.js'
+import { deleteFriend } from './friendProvider.js'
 
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector('.container')
 
 export const Friend = (friendObject) => {
-    return `
-    <div class="friend box">
-        <h3>
-            ${friendObject.username}
-        </h3>
+  return `
+    <div class="friend box padH">
+        <h3>${friendObject.username}</h3>
         <button id="deleteFriend--${friendObject.id}">X</button>
     </div>`
 }
 
-eventHub.addEventListener("click", event => {
-    if (event.target.id.startsWith("deleteFriend--")) {
-        if (window.confirm(`Are you sure you want to remove this friend?`)) {
-            const currentUserId = sessionStorage.getItem('activeUser')
-            const users = useUsers()
+eventHub.addEventListener('click', (event) => {
+  if (event.target.id.startsWith('deleteFriend--')) {
+    if (window.confirm(`Are you sure you want to remove this friend?`)) {
+      const currentUserId = sessionStorage.getItem('activeUser')
+      const users = useUsers()
 
-            const currentUserInfo = users.find(user => user.id === parseInt(currentUserId))
-            const friends = currentUserInfo.friends
+      const currentUserInfo = users.find(
+        (user) => user.id === parseInt(currentUserId)
+      )
+      const friends = currentUserInfo.friends
 
-            const [prefix, deleteFriendId] = event.target.id.split("--")
+      const [prefix, deleteFriendId] = event.target.id.split('--')
 
-            //get the friend relationship id of the friend we want to delete
-            const foundFriend = friends.find(friend => friend.following === parseInt(deleteFriendId))
+      //get the friend relationship id of the friend we want to delete
+      const foundFriend = friends.find(
+        (friend) => friend.following === parseInt(deleteFriendId)
+      )
 
-            deleteFriend(foundFriend.id)
-        }
+      deleteFriend(foundFriend.id)
     }
+  }
 })
